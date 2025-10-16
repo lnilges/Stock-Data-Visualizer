20a33af647e68b7ff992370292a99373a7f67cfa
Lani <LMNWKX@umsystem.edu>
2025-10-15 22:09:02 -0500

    created app.py & added to README

---
+++ app.py
@@ -0,0 +1,12 @@
+#This is the file to create our application
+
+#requirements
+#--------------
+    #Ask the user to enter the stock symbol for the company they want data for.
+    #Ask the user for the chart type they would like.
+    #Ask the user for the time series function they want the api to use.
+    #Ask the user for the beginning date in YYYY-MM-DD format.
+    #Ask the user for the end date in YYYY-MM-DD format.
+        #The end date should not be before the begin date
+    #Generate a graph and open in the user’s default browser.
+
