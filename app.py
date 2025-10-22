8d2b1a1965ab060943e7babf8e4c8f2d72d7c3e3
Lani <LMNWKX@umsystem.edu>
2025-10-21 21:34:41 -0500

    worked on chart visualizations

--- app.py
+++ app.py
@@ -13,11 +13,11 @@
 #API key: 9XOMX3XJU9HQH6LD
 
 import requests
-import pygal 
-import lxml
+#import pygal 
+#import lxml
 
 #Use base link and use inputs below to fill in function, symbol, and interval values
-#url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=9XOMX3XJU9HQH6LD'
+#url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=9XOMX3XJU9HQH6LD'
 #r = requests.get(url)
 #data = r.json()
 
@@ -63,16 +63,16 @@ while(True):
         try: 
             timeSeriesChoice = int(input('\nEnter time series option (1, 2, 3, 4): '))
             if(timeSeriesChoice == 1):
-                url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stockSymbol}&interval=5min&apikey=9XOMX3XJU9HQH6LD'
+                url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stockSymbol}&RANGE={startDate}&RANGE={endDate}&interval=5min&apikey=9XOMX3XJU9HQH6LD'
                 break
             elif(timeSeriesChoice == 2):
-                url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stockSymbol}&apikey=9XOMX3XJU9HQH6LD'
+                url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stockSymbol}&RANGE={startDate}&RANGE={endDate}&apikey=9XOMX3XJU9HQH6LD'
                 break
             elif(timeSeriesChoice == 3):
-                url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={stockSymbol}&apikey=9XOMX3XJU9HQH6LD'
+                url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={stockSymbol}&RANGE={startDate}&RANGE={endDate}&apikey=9XOMX3XJU9HQH6LD'
                 break
             elif(timeSeriesChoice == 4):
-                url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={stockSymbol}&apikey=9XOMX3XJU9HQH6LD'
+                url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={stockSymbol}&RANGE={startDate}&RANGE={endDate}&apikey=9XOMX3XJU9HQH6LD'
                 break
             else:
                 print("Enter a 1, 2, 3, or 4 for time series")
@@ -80,26 +80,38 @@ while(True):
             print("Enter a 1, 2, 3, or 4 for time series")
 
 
-    r = requests.get(url)
-    testData = r.json()
-    print(testData)
+    #r = requests.get(url)
+    #testData = r.json()
+    #print(testData)
 
     #Ask user for the start date
     #make sure end date is after start date
+    #need to move part with links below this or ask for start/end date within the same while loop
+    startDate = None
     while(True):
         startDate = input('\nEnter the start date (YYYY-MM-DD): ')
         break
 
 
     #Ask user for the end date, make sure it is not before the start date(datetime??)
+    endDate = None
     while(True):
         endDate = input('\nEnter the end date (YYYY-MM-DD): ')
         break
 
+
     #generate the graph
+
+    #line chart
+    #split the data from the website into lines (open, high, low, and close) and then add each to the chart 
     #pygal basic line chart
     #line_chart = pygal.Line()
     #line_chart.title = f'Stock data for {stockSymbol}'
+    #line_chart.x_labels = map(str, range(int(startDate[:4]), int(endDate[:4])))
+    #line_chart.add('name', [data])
+    #line_chart.render()
+
+    #bar chart
 
     #ask user if they want to look up more information
 
